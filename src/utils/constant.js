ae302b4a61041431ac4f0c729ca46d970bf2cd9b
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-08 23:59:30 +0530

    mock data

---
+++ src/utils/constant.js
@@ -0,0 +1,30 @@
+export const topQuote = "Every success journey we’ve encountered.";
+export const heading = "Success stories";
+export const questionAnswers = [
+  {
+    question: "Do you offer freelancers?",
+    answer:
+      "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    question:
+      "What’s the guarantee that I will be satisfied with the hired talent?",
+    answer:
+      "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    question: "Can I hire multiple talents at once?",
+    answer:
+      "If unhappy with a project, communicate with the freelancer, allow for revisions, and refer to the agreement. Escalate to platform support if needed, considering mediation. Review policies, seek collaborative solutions for resolution.",
+  },
+  {
+    question: "Why should I not go to an agency directly?",
+    answer:
+      "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    question: "Who can help me pick a right skillset and duration for me?",
+    answer:
+      "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+];
