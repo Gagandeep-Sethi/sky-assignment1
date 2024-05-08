fdcec356129f0c025e564b07ca7e6c2584ac4eb8
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-09 00:01:10 +0530

    percentcard

---
+++ src/components/homepageComponents/PercentageCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import spark from "../../assets/images/spark.png";
+const PercentageCard = ({ percentage, quote }) => {
+  return (
+    <div className="w-[267.82px] h-[239px] bg-white relative rounded-[27.11px] shadow-xl ">
+      <div className="absolute -top-4 -left-3 ">
+        <img src={spark} alt="" className="  -rotate-12" />
+      </div>
+      <div className="absolute left-9 top-9 text-lg w-[207.82px] h-[92px] ">
+        <p className="text-[64px] leading-[76.8px] ">{percentage}%</p>
+        <p className="text-zinc-500 font-manrope">{quote}</p>
+      </div>
+    </div>
+  );
+};
+
+export default PercentageCard;
