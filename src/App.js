c50d8cffe3b5c3a9b029609db3c26d6f49203ce5
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-08 23:59:16 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import SigninPage from "./components/SigninPage";
+import SuccessPage from "./components/SuccessPage";
 
 function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+  const appRounter = createBrowserRouter([
+    { path: "/", element: <HomePage /> },
+    { path: "/success", element: <SuccessPage /> },
+    { path: "/signin", element: <SigninPage /> },
+  ]);
+  return <RouterProvider router={appRounter} />;
 }
 
 export default App;
